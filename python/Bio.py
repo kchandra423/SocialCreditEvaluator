8874bf62177ce17f99d983e1ccf901b52169fabc
Kumar.s.Chandra <kchandra423@student.fuhsd.org>
2021-11-13 22:01:00 -0800

    sdf

--- python/Bio.py
+++ python/Bio.py
@@ -1,43 +1,43 @@
-from github import Github
-import os
-import requests 
-from dotenv import load_dotenv
+from github.AuthenticatedUser import AuthenticatedUser
 
-load_dotenv()
-TOKEN = os.getenv('TOKEN')
-g = Github(TOKEN)
 
-credits2 = 0 # max is 400
-
-def follow_ratio(username: str) -> int:
-    followers = g.get_user(username).followers
-    following = g.get_user(username).following
-    if (followers / following == 2 / 3):
-        credit =+ 70
+def follow_ratio(user: AuthenticatedUser) -> int:
+    credit = 0
+    followers = user.followers
+    following = max(user.following, 1)
+    if followers / following == 2 / 3:
+        credit += 70
     else:
-        credit =- 70
-    if (followers > 20 and following > 30):
-        credit =+ 50
+        credit += 70
+    if followers > 20 and following > 30:
+        credit += 50
     else:
-        credit =- 50
-
-def starring_repos(username: str) -> int:
-   starringRepos = g.get_user(username).get_starred().totalCount
-
-   if (starringRepos > 50):
-       credit =- 50
-   else:
-       credit =+ 50
+        credit -= 50
+    return credit
 
 
-def getREADME(username: str):
-    repo = g.get_user(username).get_repo(username)
+def starring_repos(user: AuthenticatedUser) -> int:
+    credit = 0
+    starringRepos = user.get_starred().totalCount
+
+    if starringRepos > 50:
+        credit -= 50
+    else:
+        credit += 50
+
+    return credit
+
+
+def getREADME(user: AuthenticatedUser):
+    credit = 0
+    repo = user.get_repo(user.name)
     file = repo.get_readme()
     file_lower = file.lower()
     # print(file.decoded_content)
     bad_words = ["founder", "ceo", "non-profit", "high school", "metaverse", "dogecoin", "taiwan"]
     for i in range:
         if (bad_words[i] in file_lower):
-            credit =- 100
+            credit -= 100
         else:
-            credit =+ 200
+            credit += 200
+    return credit
--- python/main.py
+++ python/main.py
@@ -1,6 +1,7 @@
 from RepoAnalyzer import RepoAnalyzer
 from github import Github
 import os
+import Bio
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -8,10 +9,15 @@ TOKEN = os.getenv('TOKEN')
 g = Github(TOKEN)
 
 
-
 def main(user: str):
-    scorer = RepoAnalyzer(usr=g.get_user(user))
-    print(scorer.calculate_score())
+    usr = g.get_user(user)
+    scorer = RepoAnalyzer(usr=usr)
+    social_credits = 0
+    social_credits += scorer.calculate_score()
+    social_credits += Bio.starring_repos(usr)
+    social_credits += Bio.follow_ratio(usr)
+    social_credits += Bio.getREADME(usr)
+    print(social_credits)
 
 
 if __name__ == "__main__":
