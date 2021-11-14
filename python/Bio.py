7e3b7a2d9a8573acea780ed87e7851e7bc42c442
Rebecca Wang <73043927+rbccawang@users.noreply.github.com>
2021-11-13 22:00:13 -0800

    Update Bio.py

--- python/Bio.py
+++ python/Bio.py
@@ -13,21 +13,21 @@ def follow_ratio(username: str) -> int:
     followers = g.get_user(username).followers
     following = g.get_user(username).following
     if (followers / following == 2 / 3):
-        credit =+ 70
+        credit += 70
     else:
-        credit =- 70
+        credit -= 70
     if (followers > 20 and following > 30):
-        credit =+ 50
+        credit += 50
     else:
-        credit =- 50
+        credit -= 50
 
 def starring_repos(username: str) -> int:
    starringRepos = g.get_user(username).get_starred().totalCount
 
    if (starringRepos > 50):
-       credit =- 50
+       credit -= 50
    else:
-       credit =+ 50
+       credit += 50
 
 
 def getREADME(username: str):
@@ -38,6 +38,6 @@ def getREADME(username: str):
     bad_words = ["founder", "ceo", "non-profit", "high school", "metaverse", "dogecoin", "taiwan"]
     for i in range:
         if (bad_words[i] in file_lower):
-            credit =- 100
+            credit -= 100
         else:
-            credit =+ 200
+            credit += 200
